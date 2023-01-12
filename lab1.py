2db3784df55904ef4e312ef947454c2178503bcb
Abrar Hussain <77550655+abrarhs@users.noreply.github.com>
2023-01-11 17:53:12 -0700

    download itself from GitHub

--- lab1.py
+++ lab1.py
@@ -1,6 +1,6 @@
 import requests
 
 print(requests.__version__)
-response = requests.get(http://google.com)
+response = requests.get(https://github.com/abrarhs/CMPUT404_Lab1/edit/master/lab1.py)
 print(requests.text)
 
